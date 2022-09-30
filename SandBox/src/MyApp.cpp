92f450becd766a998ed47f866b15e9564af7db9d
The big hippo <rahulkharel900@gmail.com>
2022-09-30 21:16:17 +0000

    Tesed using shader base class instead of opengl shader class to store shader

--- SandBox/src/MyApp.cpp
+++ SandBox/src/MyApp.cpp
@@ -33,18 +33,24 @@ private:
 	float m_ElapsedTime = 0;
 };
 
-const char* vertexShaderSource = "#version 330 core\n"
+const char* vertexShaderSource = 
+"#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
+"layout (location = 1) in vec3 aColor;\n"
+"out vec3 ourColor;\n"
 "void main()\n"
 "{\n"
-"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"   gl_Position = vec4(aPos, 1.0);\n"
+"   ourColor = aColor;\n"
 "}\0";
 
-const char* fragmentShaderSource = "#version 330 core\n"
+const char* fragmentShaderSource = 
+"#version 330 core\n"
 "out vec4 FragColor;\n"
+"in vec3 ourColor;\n"
 "void main()\n"
 "{\n"
-"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"   FragColor = vec4(ourColor, 1.0f);\n"
 "}\n\0";
 
 class MyApp : public cyc::Application
@@ -60,8 +66,8 @@ public:
 	void OnInit() override 
 	{	
 		window->GetLayerStack().PushBackLayer(&layer);
-		shader.Init(vertexShaderSource, fragmentShaderSource);
-		shader.Use();
+		shader->OnInit(vertexShaderSource, fragmentShaderSource);
+		shader->Use();
 	}
 
 	void OnUpdate(float elapsedTime) override 
@@ -90,7 +96,7 @@ public:
 private:
 	MyLayer layer;
 	bool m_VSyncOn = true;
-	cyc::OpenGLShader shader{};
+	std::unique_ptr<cyc::ShaderContext> shader = cyc::ShaderContext::Create();
 };
 
 cyc::Application* cyc::CreateApplication()
